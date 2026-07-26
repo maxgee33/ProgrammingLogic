714bf6ca8e81cb68243f220e4958b1aeddfebc56
maxgee33 <gee.145@osu.edu>
2026-07-26 19:28:44 +0000

    Complete Lab4

---
+++ Lab4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Car
+{
+    public string model;
+    public string color;
+    public int year;
+
+    // Example method to display car details.
+    public void Display()
+    {
+        Console.WriteLine("Car Details:");
+        Console.WriteLine("Model: " + model);
+        Console.WriteLine("Color: " + color);
+        Console.WriteLine("Year: " + year);
+    }
+    // Problem 1 //
+    public void Start()
+    {
+        Console.WriteLine("The car is starting.");
+    }
+    // Problem 2 //
+    public void Drive(int miles)
+    {
+        Console.WriteLine("The car drove " + miles + " miles.");
+    }
+    // Problem 3 //
+    public string GetDescription()
+    {
+        return year + " " + color + " " + model;
+    }
+    // Problem 4 //
+    public void Repaint(string newColor)
+    {
+        color = newColor;
+        Console.WriteLine("The car has been repainted to " + color + ".");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        // Creating a Car object and assigning values to its properties
+        Car myCar = new Car();
+        myCar.model = "Civic";
+        myCar.color = "Black";
+        myCar.year = 2020;
+
+        myCar.Display();
+        myCar.Start();
+        myCar.Drive(50);
+
+        string description = myCar.GetDescription();
+        Console.WriteLine(description);
+
+        myCar.Repaint("red");
+        Console.WriteLine("Updated Color: " + myCar.color);
+    }
+}
