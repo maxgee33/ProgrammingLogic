b9af2ad262d50ed024303f12e6b8025656bdaa36
maxgee33 <gee.145@osu.edu>
2026-07-26 18:49:21 +0000

    Complete Lab4

--- Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e2223261f3adb4e3a7f48e66fec3c26fa58a9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cd8d4311f3e5e09f629b07e87983f962adb09a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
